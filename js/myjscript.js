76f9faf058e3a6e6408c41449c471bbdf12ca9ea
mochanic <cjw5256@gmail.com>
2019-11-24 04:45:57 +0900

    webpage complete
    Brangista's MainPage of site has been completed.
    Page for Mobile is not done yet.

--- js/myjscript.js
+++ js/myjscript.js
@@ -1,3 +1,188 @@
-function ChangeNavIcon(x) {
-    x.classList.toggle("change");
+
+var newsdate = ["2019.11.15", "2019.11.14", "2019.11.14"];
+var newsnewlabel = [true, true, true];
+var newspdflabel = [false, true, true];
+var newscontent = ["中尾明慶さんが表紙・巻頭グラビアに登場！「GOODA」vol.50を公開しました。"
+                  , "2019年9月期 連結決算発表のお知らせ 10期連続増収、売上・利益すべて創業来最高を達成（PDF:135KB）"
+                   , "2019年9月期（第19期）決算短信（PDF:287KB）"
+                  ];
+
+var magazinetitle = ["tabiiro", "gooda", "superceo",
+                    "seikeidenron","madream", "gingermirror",
+                    "bonmariage" ,"bevivi" ,"nomitabi",
+                    "howb", "lista", "tabesaki",
+                    "doubleeagle"];
+var magazineinfo = ["トラベルウェブマガジン 「旅色」が、\"月刊マガジン\"になりました。 毎号、女優や著名人をゲストに迎え、大人の女性のための「旅」の魅力を伝えます。",
+                   "上質でラグジュアリーな逸品を所有する喜び。そして、それらを愛用するハイセンスな日常。そんな極上の生活を知る、モノにこだわる男のためのハイクオリティWebマガジン。",
+                   "明日を担うリーダーたちのためのビジネスマガジン。経営者の創業エピソードや製品・サービスの開発秘話、人材育成等をテーマに、ビジネスを輝かせる情報を発信。",
+                   "政治・経済の大切さを伝え、社会で役立つ情報を発信する佐藤尊徳のオピニオンメディア。歴代の政財界の知見と若い世代の感覚をぶつけて化学反応を起こし、現代を生きる若者の行動を促すことを目指します。",
+                   "「自分らしい暮らしスタイル」を指南する暮らしスタイルマガジン。もっと楽しく、自分らしく。楽しさは自分で作っていける！そんな等身大の暮らしのデザインやアイディアを「マドリーム」は提案します。",
+                   "楽天市場×GINGER のコラボで生まれた、20代後半から30代の仕事やプライベートで忙しい日々を過ごす、“時短＆コスパ派”の女性に向けた、スマホ専用の無料ファッション誌。",
+                   "プレ花嫁のためにドラマチックな結婚式を提案するウエディングマガジン。ふたりならではの個性で結婚式を演出できるよう、ウエディングドレス、ヘアメイク、マナー、おもてなしなど、トータルに紹介していきます。",
+                   "“今すぐ買える『ViVi』”というコンセプトのもと、10～20代の女性をコアターゲットに、「楽天市場」のファッションアイテムを講談社の人気雑誌『ViVi』の編集部が厳選して提案する、スマホファッションマガジン。   ",
+                   "“今すぐ買える『ViVi』”というコンセプトのもと、10～20代の女性をコアターゲットに、「楽天市場」のファッションアイテムを講談社の人気雑誌『ViVi』の編集部が厳選して提案する、スマホファッションマガジン。",
+                   "SNSを中心に自分らしさを発信するスマホネイティブ世代の「かわいくなりたい！」要望にこたえる美容情報を、発信力のある「HowBガール」とともに提案するスマホ専用のビューティーウェブマガジン。",
+                   "LiSTAは「SHOPLIST」が発信する新しいファッションマガジンです。トレンド情報やコーデのコツが満載で、しかも気になったらすぐにお買い物可能。時短＆コスパでおしゃれになりたいなら、これからはLiSTAをチェック！",
+                   "“おいしいもの”が好きなすべての人に送る、グルメ情報に特化した「旅色」の別冊です。「食」のトレンドにフォーカスした巻頭特集、旬の料理やエリアをフィーチャーした特集など、今食べたいグルメ情報が満載です。",
+                   "Double Eagleはグローバル ゴルフ メディア グループから発行される電子雑誌。旅、車、ファッション、ゴルフコース……、ラグジュアリーな大人ゴルファーに向け、新たなゴルフライフの楽しみを提案します。"];
+var magazinenewlabel = [true, true, false
+                        , false, true, false
+                        , false, false, false
+                        , false, false, true
+                        , false];
+var magazineurl = ["https://tabiiro.jp",
+                   "https://gooda.brangista.com",
+                   "https://superceo.jp",
+                   "https://seikeidenron.jp",
+                   "http://madream.jp",
+                   "http://webmagazine.brangista.com/gingermirror/",
+                   "https://b-mariage.jp",
+                   "https://r10.to/bevivi_bra",
+                   "https://nomitabi.tabiiro.jp",
+                   "https://howb.me",
+                   "https://shoplista.com",
+                   "https://tabiiro.jp/gourmet/",
+                   "https://book.golf-de.com"];
+var newsmediatitle = ["tabiiroplusweb", "tabiiroplusapp", "travelnote"];
+var newsmediainfo = ["女子のための旅情報をまとめた旅のニュースメディアです。女子が知りたい旅情報がたくさん詰まっています。最新の旅行・グルメ・おでかけ情報なら、旅色プラスで！"
+                    , "旅色プラスの無料アプリ版です。旅色プラスをもっと手軽にサクサク読みたい読者向けのiPhoneアプリです。ダウンロードでいつでも旅のトレンド情報がご覧いただけます。"
+                    , "旅行・グルメ・イベントなど、おでかけに関する幅広い情報を、短時間で読める「まとめ」形式で毎日配信している情報メディアです。ライターが旅行の楽しさを伝えるため日々情報発信しています。"];
+var newsmedianewlabel = [false, false, true];
+var newsmediaurl = ["https://plus.tabiiro.jp/"
+                   , "https://itunes.apple.com/us/app/lu-sepurasu/id1119928740?mt=8"
+                   , "https://travel-noted.jp/"];
+
+$(document).ready(function () {
+        newsinput();
+        magazinetableinput();
+        newsmediatableinput();
+    }
+);
+
+$(window).scroll(function(){
+    if(($(window).scrollTop()>100)&&($(window).scrollTop()<($(document).height()-$(window).height()-110))){
+        $("#TopButton").css("visibility", "visible");
+        $("#TopButton").stop().animate({opacity:0.7},200);
+    } else {
+        $("#TopButton").stop().animate({opacity:0},200, "linear", function(){
+        $("#TopButton").css("visibility", "hidden");
+        });
+    }
+});
+var newsnum = 0;
+function newsinput() {
+    for(var i=0;i<=2;i++) {   
+        newsnum=i+1;
+        $("#NewsArea"+newsnum+">.NewsDate").append(newsdate[i]);
+        if(newsnewlabel[i]) {
+            $("#NewsArea"+newsnum+">.NewsNewLabel").css("visibility", "visible");
+        }
+        if(newspdflabel[i]) {
+            $("#NewsArea"+newsnum+">.NewsContent").append("<span class=\"PdfLabel\">PDF</span>");
+        }
+        $("#NewsArea"+newsnum+">.NewsContent").append(newscontent[i]);
+        $("#NewsArea"+newsnum+">.NewsContent").attr("onclick", "window.open('')");
+    }
+}
+
+var magazinenum = 0;
+var magazinecnt = magazinetitle.length;
+var strmagazinedom = " ";
+function magazinetableinput() {
+    while((magazinecnt-magazinenum-1)>=3) {
+        strmagazinedom += "<tr>";
+        for(i=1;i<=3;i++) {
+            strmagazinedom += "<td><span class=\"allNewOrNot\"><img src=\"./imgs/";
+            if(magazinenewlabel[magazinenum]){
+                strmagazinedom += "isnew";
+            } else {
+                strmagazinedom += "notnew";
+            }
+            strmagazinedom += ".png\" alt=\"New or Not\" /></span><span class=\"allImageArea\" onclick=\"window.open('";
+            strmagazinedom += magazineurl[magazinenum];
+            strmagazinedom += "')\"><img src=\"./imgs/magazine/";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += "sample.png\" alt=\"";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += " sample\" /><img src=\"./imgs/magazine/";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += "logo.png\" alt=\"";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += " logo\" /></span><span class=\"TdInfo\">";
+            strmagazinedom += magazineinfo[magazinenum];
+            strmagazinedom += "</span></td>";
+
+            magazinenum++;
+        }
+    }
+    if((magazinecnt-magazinenum)>0) {
+        strmagazinedom += "<tr>";
+        for(var i=1;i<=(magazinecnt-magazinenum);i++) {
+            strmagazinedom += "<td><span class=\"allNewOrNot\"><img src=\"./imgs/";
+            if(magazinenewlabel[magazinenum]){
+                strmagazinedom += "isnew";
+            } else {
+                strmagazinedom += "notnew";
+            }
+            strmagazinedom += ".png\" alt=\"New or Not\" /></span><span class=\"allImageArea\" onclick=\"window.open('";
+            strmagazinedom += magazineurl[magazinenum];
+            strmagazinedom += "')\"><img src=\"./imgs/magazine/";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += "sample.png\" alt=\"";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += " sample\" /><img src=\"./imgs/magazine/";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += "logo.png\" alt=\"";
+            strmagazinedom += magazinetitle[magazinenum];
+            strmagazinedom += " logo\" /></span><span class=\"TdInfo\">";
+            strmagazinedom += magazineinfo[magazinenum];
+            strmagazinedom += "</span></td>";
+
+            magazinenum++;
+        }
+        strmagazinedom += "<td><span><span id=\"AndroidAppLink\" onclick=\"window.open('https://play.google.com/store/apps/details?id=com.brangista.bookshelf')\"></span><span id=\"IosAppLink\" onclick=\"window.open('https://itunes.apple.com/jp/app/%E9%9B%BB%E5%AD%90%E9%9B%91%E8%AA%8C-%E3%83%96%E3%83%A9%E3%83%B3%E3%82%B8%E3%82%B9%E3%82%BF/id986323020?mt=8')\"></span><img src=\"./imgs/applinkimg.png\" alt=\"App Link Image\" /><img src=\"./imgs/applinklogo.png\" alt=\"App Link Logo\" /></span><span class=\"TdInfo\">さまざまなジャンルのオリジナル雑誌が、スマホで簡単、手軽に読める『電子雑誌ブランジスタ』。ファッション（男/女）・ビューティ・旅行・マネー術・インテリア/住まい・政治経済等の様々なジャンルのオリジナル雑誌が毎号無料でお読みいただけます。</span></td></tr>";
+    } else {
+        strmagazinedom += "<td><span><span id=\"AndroidAppLink\" onclick=\"window.open('https://play.google.com/store/apps/details?id=com.brangista.bookshelf')\"></span><span id=\"IosAppLink\" onclick=\"window.open('https://itunes.apple.com/jp/app/%E9%9B%BB%E5%AD%90%E9%9B%91%E8%AA%8C-%E3%83%96%E3%83%A9%E3%83%B3%E3%82%B8%E3%82%B9%E3%82%BF/id986323020?mt=8')\"></span><img src=\"./imgs/applinkimg.png\" alt=\"App Link Image\" /><img src=\"./imgs/applinklogo.png\" alt=\"App Link Logo\" /></span><span class=\"TdInfo\">さまざまなジャンルのオリジナル雑誌が、スマホで簡単、手軽に読める『電子雑誌ブランジスタ』。ファッション（男/女）・ビューティ・旅行・マネー術・インテリア/住まい・政治経済等の様々なジャンルのオリジナル雑誌が毎号無料でお読みいただけます。</span></td></tr>";
+    }
+    strmagazinedom += "<tr><td class=\"allImageArea\" onclick=\"window.open('https://www.brangista.com/gyousei/')\" colspan=\"3\"><img src=\"./imgs/magpr1.png\" /></td></tr>";
+    strmagazinedom += "<tr><td class=\"allImageArea\" onclick=\"window.open('https://www.brangista.com/own_media/')\" colspan=\"3\"><img src=\"./imgs/magpr2.png\" /></td></tr>";
+    $(".MagazineTableBody").append(strmagazinedom);
+}
+
+var newsmedianum = 0;
+var strnewsmediadom = "<tr>";
+function newsmediatableinput() {
+    for(i=1;i<=3;i++) {
+        strnewsmediadom += "<td><span class=\"allNewOrNot\"><img src=\"./imgs/";
+        if(newsmedianewlabel[newsmedianum]){
+            strnewsmediadom += "isnew";
+        } else {
+            strnewsmediadom += "notnew";
+        }
+        strnewsmediadom += ".png\" alt=\"New or Not\" /></span><span class=\"allImageArea\" onclick=\"window.open('";
+        strnewsmediadom += newsmediaurl[newsmedianum];
+        strnewsmediadom += "')\"><img src=\"./imgs/newsmedia/";
+        strnewsmediadom += newsmediatitle[newsmedianum];
+        strnewsmediadom += "img.png\" alt=\"";
+        strnewsmediadom += newsmediatitle[newsmedianum];
+        strnewsmediadom += " sample\" /><img src=\"./imgs/newsmedia/";
+        strnewsmediadom += newsmediatitle[newsmedianum];
+        strnewsmediadom += "logo.png\" alt=\"";
+        strnewsmediadom += newsmediatitle[newsmedianum];
+        strnewsmediadom += " logo\" /></span><span class=\"TdInfo\">";
+        strnewsmediadom += newsmediainfo[newsmedianum];
+        strnewsmediadom += "</span></td>";
+
+        newsmedianum++;
+    }
+    strnewsmediadom += "</tr>";
+    $(".NewsMediaTableBody").append(strnewsmediadom);
+}
+
+function scrollmove(loca){
+    var offset = $(""+loca).offset();
+    $("html, body").animate({scrollTop : offset.top}, 300);
+}
+function scrolltop(){
+    $("html, body").animate({scrollTop : 0}, 300);
 }
